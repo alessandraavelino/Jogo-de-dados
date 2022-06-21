239419d1281a0037445e18e943ca909fbfbfec0b
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2022-06-21 14:54:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1
+var randomDiceeImage = "dice" + randomNumber1 + ".png";
+var randomSourceImage = "images/" + randomDiceeImage;
+image1 = document.querySelectorAll("img")[0]
+image1.setAttribute("src", randomSourceImage)
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6 ) + 1
+var randomDiceeImage2 = "dice" + randomNumber2 + ".png"
+var randomSourceImage2 = "images/" + randomDiceeImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomSourceImage2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player1 Wins";
+} 
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player2 Wins🚩";
+
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
